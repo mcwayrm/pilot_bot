abb273502076c21ac59793f1182e943b77248ab6
Ryan McWay <rmcway@dons.usfca.edu>
2019-06-21 11:52:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Pilot Bot
+#     This is designed to be a preliminary test for a survey using SurveyCTO or ODK design.
+#     It will try to break the survey design by expanding boundaries of each question.
+#     Aimed at finding the edges and outliers that could ruin the survey design.
+
+class SurveyType():
+
+class TestRun():
+
+class PilotReport():
+
